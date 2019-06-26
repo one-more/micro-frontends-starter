79f86dd3b5f6f97c758466106e9cf4c88cbcddd6
dmitrii_nikolaev <greyjoi@yandex.ru>
2019-06-26 13:40:02 +0300

    add polyfill

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import "document-register-element";
 import {define} from "@micro-frontends/core";
 import {XLink} from "@micro-frontends/framework";
 import {XContent} from "@micro-frontends/framework";
